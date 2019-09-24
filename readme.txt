ee1f999f46fcc425369aad72f208150fefd86edc
Dmitry <metey@bk.ru>
2019-09-24 16:59:32 +0300

    lesson3

---
+++ readme.txt
@@ -0,0 +1,27 @@
+====Symphony====
+php bin/console server:run
+выполните server:start и продолжайте работать с консолью дальше.
+.env настройки подключения к базе
+php bin/console doctrine:database:create  создать базу
+php bin/console make:controller PostsController
+php bin/console make:entity Post  создать сущность
+php bin/console make:migration выполнить миграцию
+php bin/console doctrine:migrations:migrate применить миграцию
+php bin/console doctrine:fixtures:load запустить наши фикстуры
+php bin/console cache:clear чистка кэша
+
+composer require --dev doctrine/doctrine-fixtures-bundle скачать DoctrineFixturesBundle
+composer require fzaninotto/faker скачаем популярную библиотеку Faker
+composer require cocur/slugify библиотека cocur/slugify, делающая slug как из английских слов, так и из русских
+
+
+{% %} делаем
+{{ }} выводим
+
+{% for post in posts %}
+ {{ post.title }}
+{% endfor %}
+
+<p>{{ post.title }} </p>   можно так делать
+
+====End Symphony====
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Cocur\Slugify\SlugifyInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    private $faker;
+
+    private $slug;
+
+    public function __construct(Slugify $slugify)
+    {
+        $this->faker = Factory::create();
+        $this->slug = $slugify;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+
+    public function loadPosts(ObjectManager $manager)
+    {
+        for ($i = 1; $i < 20; $i++) {
+            $post = new Post();
+            $post->setTitle($this->faker->text(100));
+            $post->setSlug($this->slug->slugify($post->getTitle()));
+            $post->setBody($this->faker->text(1000));
+            $post->setCreatedAt($this->faker->dateTime);
+
+            $manager->persist($post);
+        }
+        $manager->flush();
+    }
+}
